25c1a71233299447597069e9fa8b10667e98da87
Sam <samuelxborges@gmail.com>
2025-02-07 21:09:07 -0500

    Shad populated, events page front end started, started using globals for styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
-import { LatestPost } from "~/app/components/post";
+import { LatestPost } from "~/components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
-import { Button } from "./components/ui/button"; 
+import { Button } from "../components/ui/button"; 
 
 export default async function Home() {
   //const hello = await api.post.hello({ text: "WIP" });
@@ -15,7 +15,7 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#32324E] to-[#0a0a14] text-[#D9DBF1]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-background">
         <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16">
           <Image className="md:w-200 md:h-200" src="/kmodo.png" width={300} height={300} alt="kmodo" />
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
