c6da8b494cd01c43a55fe8d1a3e5365819d75296
Carlos Catala <catalacarlos23@gmail.com>
2025-03-27 22:00:51 -0400

    that didnt work, revert

--- next.config.js
+++ next.config.js
@@ -2,24 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-require('./src/env.js');
+import "./src/env.js";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import("next").NextConfig} */
+const config = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        net: false,
-        tls: false,
-        perf_hooks: false,
-        fs: false,
-      };
-    }
-    return config;
-  },
 };
 
-module.exports = nextConfig;
+export default config;
